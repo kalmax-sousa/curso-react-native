0a316865ee7973270cc6634428c7917e57b2154f
kalmax-sousa <kalmax.santos.123@gmail.com>
2020-11-07 15:06:06 -0300

    Componentização

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
 function App() {
   return (
-    <h1> Olá Mundo! </h1>
+    <>
+      <Header/>
+      <Header/>
+      <Header/>
+      <Header/>
+    </>
   );
 }
 
